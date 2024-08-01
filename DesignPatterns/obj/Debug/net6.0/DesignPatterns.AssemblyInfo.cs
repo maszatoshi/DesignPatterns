9cd58305f0f5f9bebe6cc903b42d53e80d4ec2f2
maszatoshi <maszatoshi@gmail.com>
2024-08-01 10:32:00 +0200

    del

--- DesignPatterns/obj/Debug/net6.0/DesignPatterns.AssemblyInfo.cs
+++ DesignPatterns/obj/Debug/net6.0/DesignPatterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b256aecc0236dec117e245b3a5d6f84ea593c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
